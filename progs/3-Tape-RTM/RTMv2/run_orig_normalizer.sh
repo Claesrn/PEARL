ee2c373cbc4343432f79f2c4a38e15cdc4703e74
Claes2840 <Claes.r.nielsen@gmail.com>
2026-02-16 23:29:35 +0100

    Increment works :D

--- progs/3-Tape-RTM/RTMv2/run_orig_normalizer.sh
+++ progs/3-Tape-RTM/RTMv2/run_orig_normalizer.sh
@@ -5,18 +5,18 @@ set -e
 cd "$(dirname "$0")"
 
 # 1. Run BinInc and BinDec (if needed)
-stack run -- interpret RTMinv.rl orig-normalizer/BinInc.spec
-stack run -- interpret RTM.rl orig-normalizer/BinDec.spec
+stack run -- interpret RTMinv.rl increment/BinInc.spec
+stack run -- interpret RTM.rl increment/BinDec.spec
 # (Assume BinInc.out and BinDec.out already exist)
 
 # 2. Specialize BinInc and BinDec
-stack run -- spec RTMinv.rl orig-normalizer/BinInc.out orig-normalizer/BinInc_comment.spec
-stack run -- spec RTM.rl orig-normalizer/BinDec.out orig-normalizer/BinDec_comment.spec
+stack run -- spec RTMinv.rl increment/BinInc.out increment/BinInc_comment.spec
+stack run -- spec RTM.rl increment/BinDec.out increment/BinDec_comment.spec
 
 # 3. Normalize with original normalizer
-stack run -- normalize orig-normalizer/BinInc.out orig-normalizer/BinInc_orig.norm
-stack run -- normalize orig-normalizer/BinDec.out orig-normalizer/BinDec_orig.norm
+stack run -- normalize increment/BinInc.out increment/BinInc.norm
+stack run -- normalize increment/BinDec.out increment/BinDec.norm
 
-echo "Diff (should show 3 cosmetic differences):"
-diff orig-normalizer/BinInc_orig.norm orig-normalizer/BinDec_orig.norm || true
+echo "Diff below:"
+diff increment/BinInc.norm increment/BinDec.norm || true
 
